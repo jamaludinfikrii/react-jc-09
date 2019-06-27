4ceba3889f43816105e264f5180baa32115c01e3
fikri <jamaludinfikrii@gmail.com>
2019-06-27 11:49:26 +0700

    api wilayah

--- latihan-react/src/components/apiWilayah.jsx
+++ latihan-react/src/components/apiWilayah.jsx
@@ -1,21 +1,85 @@
 import React, { Component } from 'react'
 import {Input} from 'reactstrap'
+import Axios from 'axios';
+
+const url = 'https://x.rajaapi.com/MeP7c5neRZPPYYYeRvsoXaByj8v52nAfAvwm2BYhRdf4soquXNE7729t1F/m/wilayah'
 
 class Wilayah extends Component {
+    state = {
+        provinsi : [],
+        kabupaten : [],
+        loadingKabupaten : true,
+    }
+    componentDidMount(){
+        Axios.get(url + '/provinsi')
+        .then((res) => {
+            console.log(res.data.data)
+            this.setState({provinsi : res.data.data})
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
+    renderDataProvinsi = () => {
+        var jsx = this.state.provinsi.map((val) => {
+            return (
+                <option value={val.id}> {val.name} </option>
+            )
+        })
+
+        return jsx
+    }
+
+    handleOnClickKabupaten = () => {
+        var id = this.refs.provinsi.refs.provinsiInner.value
+        Axios.get(url + '/kabupaten?idpropinsi=' + id)
+        .then((res) => {
+            this.setState({kabupaten : res.data.data , loadingKabupaten : false})
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
+    renderDataKabupaten = () => {
+        if(this.state.loadingKabupaten === true){
+            return (
+                <option> loading ... </option>
+            )
+        }
+        var jsx = this.state.kabupaten.map((val) => {
+            return (
+                <option> {val.name} </option>
+            )
+        })
+        return jsx
+    }
+    onChangeProvinsi = () => {
+        this.setState({loadingKabupaten : true})
+    }
+
     render() {
+        if(this.state.provinsi.length === 0){
+            return (
+                <h1> Loading ... </h1>
+            )
+        }
         return (
             <div className='container'>
                 <h1>API Wilayah Indonesia</h1>
                 <div className='row justify-content-center'>
                     <div className='col-md-4'>
-                        <Input type='select' >
+                        <Input ref='provinsi' onChange={this.onChangeProvinsi} innerRef='provinsiInner' type='select' >
                             <option>Pilih Provinsi</option>
+                            {this.renderDataProvinsi()}
                         </Input>
                     </div>
 
                     <div className='col-md-4'>
-                        <Input type='select' >
+                        <Input onClick={this.handleOnClickKabupaten} type='select' >
                             <option>Pilih Kabupaten</option>
+                            {this.renderDataKabupaten()}
                         </Input>
                     </div>
                 </div>
